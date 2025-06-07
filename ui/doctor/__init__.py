90be028ef24689d174ad96d832acd28d55e48075
Yousef Mohsen <yousef2002316@gmail.com>
2025-06-08 01:10:55 +0300

    improved

--- ui/doctor/__init__.py
+++
@@ -1,4 +0,0 @@
-from .profile import DoctorProfilePage
-from .patients import MyPatientsPage
-from .schedule import DoctorSchedulePage
-from .write_prescription import WritePrescriptionPage
--- ui/patient/__init__.py
+++
@@ -1,6 +0,0 @@
-from .profile import PatientProfilePage
-from .appointments import PatientAppointmentsPage
-from .prescriptions import PatientPrescriptionsPage
-from .medical_records import PatientMedicalRecordsPage
-from .book_appointment import BookAppointmentDialog
-from .billing import PatientBillingPage
